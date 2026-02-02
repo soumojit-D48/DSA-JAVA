8fbe0ed9ca66f6369d6391fd688e982c62c471d2
soumojit-D48 <soumojitdas107@gmail.com>
2026-02-02 23:32:26 +0530

    Greedy algo 678

---
+++ Leetcode/Greedy/Q678.java
@@ -0,0 +1,24 @@
+public class Q678 {
+    public boolean checkValidString(String s) {
+        int low = 0, high = 0;
+
+        for (char c : s.toCharArray()){
+            if(c == '('){
+                low++;
+                high++;
+            } 
+            else if(c == ')'){
+                low--;
+                high--;
+            }
+            else {
+                low--;
+                high++;
+            }
+
+            if(high < 0) return false;
+            if(low < 0) low = 0;
+        }
+        return low == 0;
+    }
+}
